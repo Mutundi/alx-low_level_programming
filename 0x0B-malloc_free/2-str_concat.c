cb357e194098d762e9dbe982d5f7f4f07ecd61db
Mutundi Levi <levimutundi@gmail.com>
2023-09-06 13:57:03 +0300

    Write a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - calculates the length of the string.
+ * @s: the string.
+ * Return: 0 for success.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * str_concat - concatenates two strings.
  * @s1: the first string.
  * @s2: the second string.
@@ -15,36 +33,23 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, m, n;
 	char *dest;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-	dest = (char *)malloc((len1 + len2) * sizeof(char) + 1);
+	dest = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (dest == NULL)
-	{
 		return (NULL);
-	}
 
-	for (n = 0; n < len1; n++)
-	{
-		dest[n] = s1[n];
-	}
-	for (m = 0; m < len2; m++)
-	{
-		dest[n + m] = s2[m];
-	}
+	for (m = 0; m < len1; m++)
+		dest[m] = s1[m];
+	for (n = 0; n < len2; n++)
+		dest[n + m] = s2[n];
 
 	dest[n + m] = '\0';
 	return (dest);
